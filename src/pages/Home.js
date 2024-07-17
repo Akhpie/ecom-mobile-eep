361d9404271300ec6937e6b93a06b02065ae212e
Akhpie <134225827+Akhpie@users.noreply.github.com>
2024-07-17 23:25:05 +0530

    new changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,13 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import ProductCard from '../components/ProductCard';
 import Cart from './Cart';
+import iphoneImage from '../assets/iphone 15 no bg.png'
+import iphonePink from '../assets/iphone15pink.png'
 
 const products = [
-  { id: 1, name: 'Product 1', price: 10, image: 'https://via.placeholder.com/150' },
-  { id: 2, name: 'Product 2', price: 20, image: 'https://via.placeholder.com/150' },
+  { id: 1, name: 'Product 1', price: 10, image: iphoneImage },
+  { id: 2, name: 'Product 2', price: 20, image: iphonePink },
+  { id: 3, name: 'Product 2', price: 20, image: iphonePink },
 ];
 
 const Home = ({ cart, setCart }) => {
